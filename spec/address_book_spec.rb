1924511c8c0121479d8c8a0da233aeced4c5f374
Jon Reck <jonathonreck@gmail.com>
2017-12-21 15:57:10 -0500

    remove_entry test passing

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,14 @@ require_relative '../models/address_book'
         expect(new_entry.email).to eq('augusta.king@lovelace.com')
       end
     end
+
+    describe "#remove_entry" do
+      it "removes the correct entry from address book" do
+        book = AddressBook.new
+        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+        book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+        expect(book.entries.size).to eq(0)
+      end
+    end
 end
